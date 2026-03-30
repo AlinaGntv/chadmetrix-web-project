e3b72fdaf1e5e99046ba439ca699d304f5eaf7d0
Ваше имя <alinaperova08@gmail.com>
2026-03-30 14:38:17 +0300

    REF

--- chadmetrix-web/lib/api.ts
+++ chadmetrix-web/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "/api",
+    baseURL: "",  // ← пустой, т.к. Nginx проксирует /api/ → backend
     headers: {
         "Content-Type": "application/json",
     },
